dd4e822ebe3cfa1f6bbffc4d5be0ac3a4f85ce6d
Innovate Core <kolaymaksimiv@gmail.com>
2025-01-28 19:50:45 +0200

    Interfaces and Extensibility

--- CSharpIntermediate/Sections/Section5.cs
+++ CSharpIntermediate/Sections/Section5.cs
@@ -1,4 +1,5 @@
-﻿using static CSharpIntermediate.Sections.Section5;
+﻿using static CSharpIntermediate.Sections.Section2;
+using static CSharpIntermediate.Sections.Section5;
 
 namespace CSharpIntermediate.Sections;
 
@@ -64,9 +65,79 @@ public static class Section5
         }
     }
 
+    //////////////////////////////////////////////////////////////////////////////////////
+    public class ConsoleLogger : ILogger
+    {
+        public void LogError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+        }
+    }
+
+    public class DbMigrator
+    {
+        private readonly ILogger _logger;
+
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.LogInfo("Migrationg started at {0}" + DateTime.Now);
+
+            // Details of migrating the database
+
+            _logger.LogInfo("Migrationg finished at {0}" + DateTime.Now);
+        }
+    }
+
+    public class FileLogger : ILogger
+    {
+        private readonly string _path;
+
+        public FileLogger(string path)
+        {
+            _path = path;
+        }
+
+        public void LogError(string message)
+        {
+            Log(message, "ERROR");
+        }
+
+        public void LogInfo(string message)
+        {
+            Log(message, "INFO");
+        }
+
+        private void Log(string message, string messageType)
+        {
+            using (var streamWriter = new StreamWriter(_path, true))
+            {
+                streamWriter.WriteLine(messageType + ": " + message);
+            }
+        }
+    }
+
+    public interface ILogger
+    {
+        void LogError(string message);
+        void LogInfo(string message);
+    }
+
     public static void Run()
     {
         InterfacesAndTestability();
+        InterfacesAndExtensibility();
     }
 
     private static void InterfacesAndTestability()
@@ -78,5 +149,15 @@ public static class Section5
         orderProcessor.Process(order);
 
         Console.WriteLine("Finish -> Interfaces and Testability");
+    }
+
+    private static void InterfacesAndExtensibility()
+    {
+        Console.WriteLine("Start -> Interfaces and Extensibility");
+
+        var dbMigrator = new DbMigrator(new FileLogger("C:\\Projects\\log.txt"));
+        dbMigrator.Migrate();
+
+        Console.WriteLine("Finish -> Interfaces and Extensibility");
     }
 }
